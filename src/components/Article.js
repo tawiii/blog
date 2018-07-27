1f675b7fdcb2f8dcd926bd8f161eeb5bd504715e
tawiii <4etkiu94@mail.ru>
2018-07-27 15:21:41 +0300

    blog

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 
 class Article extends Component {
-  
+
   render() {
     const {article} = this.props
 
     return (
       <div>
         <h3>
-          <Link to={`blog/${article.id}`}>
+          <Link to={`/blog/${article.id}`}>
             {article.title}
           </Link>
         </h3>        
